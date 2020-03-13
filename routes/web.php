ba2cf994bdd0d14da58e666da3f1ea10b4d1d93e
Fergie <akinfergie@gmail.com>
2020-03-14 00:51:44 +0800

    Updating routes for the controllers

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,36 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+// Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['prefix' =>'admin', 'middleware' => 'auth'], function(){
+
+    Route::get('/home', 'HomeController@index')->name('home');
+    
+    Route::resource('/post', 'PostsController');
+
+    // Route::get('/post/create', 'PostsController@create')->name('post.create');
+      
+    // Route::post('/post/store', 'PostsController@store')->name('post.store');
+
+    Route::resource('/category', 'CategoriesController');
+
+    Route::get('/posts/trashed', 'PostsController@trashed')->name('post.trashed');
+    Route::delete('/posts/kill/{post}', 'PostsController@kill')->name('post.kill');
+    Route::get('/posts/restore/{post}', 'PostsController@restore')->name('post.restore');
+
+
+    // Route::get('/category/create', 'CategoriesController@create')->name('category.create');
+
+    // Route::get('/category/edit/{id}', 'CategoriesController@edit')->name('category.edit');
+
+    // Route::get('/category/delete/{id}', 'CategoriesController@destroy')->name('category.delete');
+
+    // Route::post('/category/store', 'CategoriesController@store')->name('category.store');
+
+
+});
+
