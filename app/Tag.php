7a63d898511f28b3419955cf2b49b561f66ee34e
Fergie <akinfergie@gmail.com>
2020-03-14 04:32:24 +0800

    Added Tag model

---
+++ app/Tag.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Post;
+
+class Tag extends Model
+{
+    protected $fillable = ['tag'];
+  
+    public function posts(){
+
+        return $this->hasMany('Post');
+    }
+}
