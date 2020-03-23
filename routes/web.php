91452fa4f26f8b4a348e0e811af5f70ebff4ff0a
Fergie <akinfergie@gmail.com>
2020-03-23 18:59:05 +0800

    Created resources and custom routes for the views

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'FrontendController@index')->name('index');
+Route::get('/posts/{slug}', 'FrontendController@singlepost')->name('single');
+Route::get('/results', 'FrontendController@result')->name('result');
+
+Route::get('/categories/{category}', 'FrontendController@singleCategory')->name('single.category');
+Route::get('/tags/{tag}', 'FrontendController@singleTag')->name('single.tag');
+
 
 Auth::routes();
 
@@ -27,26 +31,26 @@ Route::group(['prefix' =>'admin', 'middleware' => 'auth'], function(){
     
     Route::resource('/post', 'PostsController');
 
-    // Route::get('/post/create', 'PostsController@create')->name('post.create');
-      
-    // Route::post('/post/store', 'PostsController@store')->name('post.store');
-
+    
     Route::resource('/category', 'CategoriesController');
     Route::resource('/tag', 'TagsController');
+    Route::resource('/user', 'UsersController');
 
+    Route::get('/setting', 'SettingsController@index')->name('settings.setting');
+    Route::post('/setting/update', 'SettingsController@update')->name('settings.update');
+
+    Route::get('/user/profile/{user}', 'UsersController@edit')->name('user.profile.edit');;
+
+    Route::get('/users/trashed', 'UsersController@trashed')->name('user.trashed');
     Route::get('/posts/trashed', 'PostsController@trashed')->name('post.trashed');
     Route::delete('/posts/kill/{post}', 'PostsController@kill')->name('post.kill');
+    Route::delete('/users/kill/{post}', 'UsersController@kill')->name('user.kill');
+
     Route::get('/posts/restore/{post}', 'PostsController@restore')->name('post.restore');
+    Route::get('/users/restore/{post}', 'UsersController@restore')->name('user.restore');
 
 
-    // Route::get('/category/create', 'CategoriesController@create')->name('category.create');
-
-    // Route::get('/category/edit/{id}', 'CategoriesController@edit')->name('category.edit');
-
-    // Route::get('/category/delete/{id}', 'CategoriesController@destroy')->name('category.delete');
-
-    // Route::post('/category/store', 'CategoriesController@store')->name('category.store');
-
-
+    Route::get('/users/admin/{user}', 'UsersController@admin')->name('user.admin');
+    Route::get('/users/notadmin/{user}', 'UsersController@notAdmin')->name('user.notadmin');
 });
 
